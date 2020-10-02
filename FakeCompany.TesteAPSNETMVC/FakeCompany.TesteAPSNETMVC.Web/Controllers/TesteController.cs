c478bccc8a5a901396fd2f73664fdcea1612659f
Fabricio Cirino <f.alves.cirino@avanade.com>
2020-10-02 07:29:57 -0300

    Primeiro commit

---
+++ FakeCompany.TesteAPSNETMVC/FakeCompany.TesteAPSNETMVC.Web/Controllers/TesteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeCompany.TesteAPSNETMVC.Web.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection formCollection)
+        {
+
+            ViewBag.Mensagem = formCollection["nome"];
+            return View("Saudacao");
+        }
+    }
+}
